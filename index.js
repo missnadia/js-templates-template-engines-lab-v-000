1dee4167a70a89a41e797ea58c8c069ba7938a80
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-20 21:03:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function createPost() {
   var postForm = document.getElementById("post-form");
   var pageTemplate = document.getElementById("page-template").innerHTML;
   var postTemplate = document.getElementById("post-template").innerHTML;
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
 }
 
 function postComment() {
