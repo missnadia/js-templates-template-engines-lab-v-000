ade54f57f4f7f66ab008114a171ed43d695f7a76
Nadia Lee <miss.nadia.lee@gmail.com>
2018-09-20 20:30:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function postComment() {
   var commenter = document.getElementById('commenterName').value;
   var comment = document.getElementById('commenterText').value;
+  var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
 }
